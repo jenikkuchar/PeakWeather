5fff9e0a52be3933c29ed57a218055e5fecfce54
jenikkuchar <91950308+jenikkuchar@users.noreply.github.com>
2025-02-28 09:23:22 +0100

    Update __init__.py

--- peaks/__init__.py
+++ peaks/__init__.py
@@ -1,1 +1,6 @@
+# Inicializační soubor pro balíček peaks
+from peaks.lysa_hora import get_lysa_hora_data
+from peaks.pustevny import get_pustevny_data
+from peaks.velky_javornik import get_velky_javornik_data
 
+__all__ = ['get_lysa_hora_data', 'get_pustevny_data', 'get_velky_javornik_data']
